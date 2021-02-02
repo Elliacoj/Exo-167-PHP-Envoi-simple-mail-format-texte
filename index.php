0e837fc0d6743c071d53f779c4223052c581944a
Elliacoj <amaury.jocaille@hotmail.com>
2021-02-02 16:19:43 +0100

    Modif

--- index.php
+++ index.php
@@ -8,9 +8,9 @@
 
 $from = '';
 $to = '';
-$message = 'Hello World, sending a simple mail !';
+//$message = 'Hello World, sending a simple mail !';
 // TODO Votre code ici.
-
+//mail($to, "Premier mail", $message);
 
 /**
  * 4. Commentez le code précédent, mais gardez les variables $from et $to
@@ -24,3 +24,13 @@ $message = 'Hello World, sending a simple mail !';
  *     N'écrasez pas les valeurs déjà existantes ( s'il y en a ).
  */
 // TODO Votre code ici.
+
+$message = "Je suis le nouveau message à envoyer par mail pour tester et faire en sorte que ça fonctionne. Je pense qu'il y a assez de caractères.";
+$message = wordwrap($message, 70, "\r\n");
+if(mail($to, "Premier mail", $message)) {
+    echo "Message bien envoyé";
+    file_put_contents('mails.txt', '\n' . $to . ": " . $message, FILE_APPEND);
+}
+else {
+    echo "L'envoi du message a échoué";
+}
